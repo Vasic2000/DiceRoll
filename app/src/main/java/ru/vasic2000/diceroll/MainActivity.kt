0893859ad7d4b01235b4352a33b666982e771ce5
vasic2000 <vasic2000@yandex.ru>
2022-09-16 21:48:18 +0300

    Кубики

--- app/src/main/java/ru/vasic2000/diceroll/MainActivity.kt
+++ app/src/main/java/ru/vasic2000/diceroll/MainActivity.kt
@@ -2,10 +2,31 @@ package ru.vasic2000.diceroll
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollTextView : TextView = findViewById(R.id.textView)
+        val rollButton = findViewById<Button>(R.id.button)
+
+        rollButton.setOnClickListener{
+            rollTextView.setText(rollDice().toString())
+        }
+    }
+
+    private fun rollDice() : Int {
+        val dice = Dice(6)
+        return dice.roll()
+    }
+
+    class Dice(val numSides : Int) {
+        fun roll() : Int {
+            return(1..numSides).random()
+        }
     }
 }
