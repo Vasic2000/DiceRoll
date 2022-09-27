f27d02eb435fd5a40c756f265a5d911a9ee93dcf
vasic2000 <vasic2000@yandex.ru>
2022-09-27 21:50:53 +0300

    Лучше

--- app/src/main/java/ru/vasic2000/diceroll/SecondThread.kt
+++ app/src/main/java/ru/vasic2000/diceroll/SecondThread.kt
@@ -207,7 +207,65 @@ class SecondThread(private val mainActivity: MainActivity) : Runnable {
         }
 
         try {
-            Thread.sleep(500)
+            Thread.sleep(200)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+        side  = rollDice()
+
+        when (side) {
+            1 -> mainActivity.diceImageView.post{
+                mainActivity.diceImageView.setImageResource(R.drawable.dice1)
+            }
+            2 -> mainActivity.diceImageView.post {
+                mainActivity.diceImageView.setImageResource(R.drawable.dice2)
+            }
+            3 -> mainActivity.diceImageView.post {
+                mainActivity.diceImageView.setImageResource(R.drawable.dice3)
+            }
+            4 -> mainActivity.diceImageView.post {
+                mainActivity.diceImageView.setImageResource(R.drawable.dice4)
+            }
+            5 -> mainActivity.diceImageView.post {
+                mainActivity.diceImageView.setImageResource(R.drawable.dice5)
+            }
+            6 -> mainActivity.diceImageView.post {
+                mainActivity.diceImageView.setImageResource(R.drawable.dice6)
+            }
+        }
+
+        try {
+            Thread.sleep(150)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
+        side  = rollDice()
+
+        when (side) {
+            1 -> mainActivity.diceImageView.post{
+                mainActivity.diceImageView.setImageResource(R.drawable.dice1)
+            }
+            2 -> mainActivity.diceImageView.post {
+                mainActivity.diceImageView.setImageResource(R.drawable.dice2)
+            }
+            3 -> mainActivity.diceImageView.post {
+                mainActivity.diceImageView.setImageResource(R.drawable.dice3)
+            }
+            4 -> mainActivity.diceImageView.post {
+                mainActivity.diceImageView.setImageResource(R.drawable.dice4)
+            }
+            5 -> mainActivity.diceImageView.post {
+                mainActivity.diceImageView.setImageResource(R.drawable.dice5)
+            }
+            6 -> mainActivity.diceImageView.post {
+                mainActivity.diceImageView.setImageResource(R.drawable.dice6)
+            }
+        }
+
+        try {
+            Thread.sleep(150)
         } catch (e: InterruptedException) {
             e.printStackTrace()
         }
