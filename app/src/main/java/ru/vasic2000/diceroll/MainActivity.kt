906e69defe81262bebb3d7a7c2bd303cc5286e43
vasic2000 <vasic2000@yandex.ru>
2022-10-18 13:20:30 +0300

    Bottom Navigation 3 фрагмента работает

--- app/src/main/java/ru/vasic2000/diceroll/MainActivity.kt
+++ app/src/main/java/ru/vasic2000/diceroll/MainActivity.kt
@@ -1,22 +1,46 @@
 package ru.vasic2000.diceroll
 
-import android.media.AudioManager
-import android.media.SoundPool
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+
 import android.widget.Button
 import android.widget.ImageView
 
+import android.media.AudioManager
+import android.media.SoundPool
+import ru.vasic2000.diceroll.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     lateinit var diceImageView : ImageView
 
     //    Sounds
-    lateinit var sounds : SoundPool
-    var soundThrow : Int = 0
+    private lateinit var sounds : SoundPool
+    private var soundThrow : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_dice_1, R.id.navigation_dice_2, R.id.navigation_dice_4
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
         diceImageView = findViewById(R.id.diceImage)
         val rollButton = findViewById<Button>(R.id.button)
@@ -45,7 +69,7 @@ class MainActivity : AppCompatActivity() {
         return dice.roll()
     }
 
-    class Dice(val numSides : Int) {
+    class Dice(private val numSides : Int) {
         fun roll() : Int {
             return(1..numSides).random()
         }
